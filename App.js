65c6e8d1d5060253470868c27bafe19fde871ef0
PauloPZ <pc.zuit@gmail.com>
2024-03-27 11:00:38 -0300

    atualização

--- App.js
+++ App.js
@@ -89,8 +89,8 @@ export default function App() {
           <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, marginBottom: 15}}>Com os preços:</Text>
           <Text style={styles.textoprecos}>Álcool: R$ {num1}</Text>
           <Text style={styles.textoprecos}>Gasolina: R$ {num2}</Text>
-          <TouchableOpacity onPress={() => visModal(false)} style={{width: '65%', height: '5%', borderColor: 'red', borderWidth: 1, borderRadius: 5, justifyContent: 'center', marginTop: 15}} >
-            <Text style={{fontSize: '30', fontWeight: 'bold', color: 'red', textAlign: 'center'}}>Caucular novamente</Text>
+          <TouchableOpacity onPress={() => visModal(false)} style={{width: '65%', height: '12%', borderColor: 'red', borderWidth: 1, borderRadius: 5, justifyContent: 'center', marginTop: 15}} >
+            <Text style={{fontSize: 30, fontWeight: 'bold', color: 'red', textAlign: 'center'}}>Calcular novamente</Text>
           </TouchableOpacity>
         </View>
       </Modal>
